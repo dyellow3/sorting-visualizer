77d14390394b19ff42ccdd01e0a375546df46cc8
dyellow3 <dyellow3@asu.edu>
2023-12-01 23:50:18 -0700

    cleaned code

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 import D3Chart from './Components/D3Chart'
-import { shuffle } from 'd3';
 
 function App() {
-  var dataarr = [...Array(10).keys()];;
-
-  shuffle(dataarr)
-
   return (
-    <>
-      <div className="header">
-
-      </div>
-      <main>
-        <D3Chart data={dataarr} />
-      </main>
-      
-    </>
+        <D3Chart />
   )
 }
 
