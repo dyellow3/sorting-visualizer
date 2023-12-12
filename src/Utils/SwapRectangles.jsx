21a358e68b1fc192f5c1a4668bf8d9168057ec5f
dyellow3 <dyellow3@asu.edu>
2023-12-11 22:53:09 -0700

    rectangles swap by taking each others place

--- src/Utils/SwapRectangles.jsx
+++ src/Utils/SwapRectangles.jsx
@@ -1,45 +1,5 @@
 import * as d3 from 'd3';
 import { CHART_WIDTH, CHART_HEIGHT } from '../Components/config';
-/*
-export const SwapRectangles = (index1, index2, data, speed) => {
-    return new Promise((resolve) => {
-        const barWidth = (CHART_WIDTH - 50) / data.length;
-        const barHeight = (CHART_HEIGHT - 10) / data.length;
-
-        const temp = data[index1]
-        data[index1] = data[index2]
-        data[index2] = temp
-        
-        const rect1 = d3.select('rect[index="' + index1 + '"]');
-        const rect2 = d3.select('rect[index="' + index2 + '"]');
-
-        // Get the y and height values for the rectangles
-        const y1 = parseFloat(rect1.attr('y'));
-        const height1 = parseFloat(rect1.attr('height'));
-        const y2 = parseFloat(rect2.attr('y'));
-        const height2 = parseFloat(rect2.attr('height'));
-
-        // Swap rectangles visually
-        // make rect1 have rect2s index, and rect2 have rect1s
-        // make rect1 move to rect2s pos, rect2 to rect1s pos
-        rect1
-            .transition()
-            .duration(speed * 100 + 50)
-            .attr('y', y2)
-            .attr('height', height2);
-
-        rect2
-            .transition()
-            .duration(speed * 100 + 50)
-            .attr('y', y1)
-            .attr('height', height1);
-
-        // Add a delay after swapping
-        setTimeout(() => {
-            resolve();
-        }, speed * 100 + 75); // +75 to make sure enough time
-    });
-};*/
 
 export const SwapRectangles = (index1, index2, data, speed) => {
     return new Promise((resolve) => {
